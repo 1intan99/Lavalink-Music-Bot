c08cdcd368297d57f7f11bcc5247dbc7f187c21f
ameliakiara <lintanggaming24@gmail.com>
2021-12-25 19:16:52 +0700

    update: update permission invite code for each bot

--- src/route/index.ts
+++ src/route/index.ts
@@ -3,8 +3,8 @@ import { Router } from "express";
 export const router = Router()
     .get('/', (req, res) => res.render("kiara"))
     .get('/bot', (req, res) => res.render("bot"))
-    .get('/nao', (req, res) => res.redirect("https://discord.com/oauth2/authorize?client_id=904209927415930950&permissions=155629447415&scope=bot%20applications.commands"))
-    .get('/nao-tomori', (req, res) => res.redirect("https://discord.com/oauth2/authorize?client_id=492600047268134912&permissions=155629447415&scope=bot%20applications.commands"))
-    .get('/narberal', (req, res) => res.redirect("https://discord.com/oauth2/authorize?client_id=480644131782131732&permissions=155629447415&scope=bot%20applications.commands"));
+    .get('/nao', (req, res) => res.redirect("https://discord.com/oauth2/authorize?client_id=904209927415930950&scope=applications.commands%20bot&permissions=439663587071"))
+    .get('/nao-tomori', (req, res) => res.redirect("https://discord.com/oauth2/authorize?client_id=492600047268134912&scope=applications.commands%20bot&permissions=439663587071"))
+    .get('/narberal', (req, res) => res.redirect("https://discord.com/oauth2/authorize?client_id=480644131782131732&scope=applications.commands%20bot&permissions=439663587071"));
 
 export default router;
